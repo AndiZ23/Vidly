ca13b45f4acaf41a0e778b92d0af3d53613ece28
Andi Zhou <zhouandi423@gmail.com>
2018-07-01 23:29:04 -0600

    Some key points for ActionResults (added as comments)

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -20,6 +20,33 @@ namespace Vidly.Controllers
             return View(movie); // pass the instance/data to the view
             // the controller passes data (that formed by the Model -- instance) to the View, 
             //  which has to be the same name as this method, aka. Random.cshtml.
+
+            /*  Examples of different ActionResults   */
+            //return Content("Hello World!");   // empty page with the plain text on it.
+            //return HttpNotFound();    // 404 page
+            //return new EmptyResult(); // a empty blank page
+            //return RedirectToAction("Index","Home", new {page = 1. sortBy="name"});
+                // parameters: 1st. the name of the action; 2nd. destination page of the redirction; 
+                //             3rd. the data passing during the redirect. 
+                // Output: redirect to Home page, with page=1&sortBy=name on the url (like GET data)
+
         }
     }
 }
+// ActionResult:
+//      The base class for all action resolves in ASP.NET MVC
+//      One of the subclass: ViewResult
+//          Alternatively, the return can change to: return new ViewResult();
+//      But since ActionResult is the parent class, so by using this, we can return any of its subtypes.
+
+// All ActionResult's alternatives:
+//      Type                    Helper Method           function
+//      ViewResult              View()                  To return a View
+//      PartialViewResult       PartialView()           To return a partial view
+//      ContentResult           Content()               To return simple text redirect result
+//      RedirectResult          Redirect()              To redirect the user to a url
+//      RedirectToRouteResult   RedirectToAction()      To redirect to an action instead of a url
+//      JsonResult              Json()                  To return a json object
+//      FileResult              File()                  To return a file
+//      HttpNotFoundResult      HttpNotFound()          To return an error or 404
+//      EmptyResult             N/A                     No return needed
