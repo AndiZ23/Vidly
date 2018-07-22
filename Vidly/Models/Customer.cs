3f957b8adb2fc9cf183b831ecbfa8e30c833e77a
Andi Zhou <zhouandi423@gmail.com>
2018-07-22 16:49:51 -0600

    Alter column types and insert data using code-first approach.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Required]  // becomes non-nullable in the database.
+        [StringLength(255)]
+        public string Name { get; set; }  // string will be nullable and max length in the database on default; using the [] to re-write it.
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; } // foreign key to the MembershipType
